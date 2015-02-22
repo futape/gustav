50ed12a8ad515e9b24f48fc1cf483422b52885ed
Lucas Krause <info@futape.de>
2015-02-22 10:34:55 +0100

    bump version to 1.1.0

--- src/futape/gustav/GustavBase.php
+++ src/futape/gustav/GustavBase.php
@@ -1,5 +1,5 @@
 <?php
-/*! Gustav 1.0.0 | Copyright (c) 2015 Lucas Krause | New BSD License | http://gustav.futape.de */
+/*! Gustav 1.1.0 | Copyright (c) 2015 Lucas Krause | New BSD License | http://gustav.futape.de */
 
 namespace futape\gustav;
 
